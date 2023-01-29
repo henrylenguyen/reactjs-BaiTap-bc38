e9576ff10813adf17625ba694597ee340f6c50b4
Le Nguyen Phuong Thai <thailnp133@gmail.com>
2023-01-29 20:15:18 +0700

    tối ưu giao diện

--- src/layouts/MovieSeats/TicketDetails.js
+++ src/layouts/MovieSeats/TicketDetails.js
@@ -9,14 +9,14 @@ const TicketDetails = () => {
   const { bookedSeatList } = useSelector((state) => state.MovieSeats);
   const dispatch = useDispatch();
   return (
-    <div className="sm: p-[2rem] grid grid-cols-5 gap-5 md:p-[3rem] lg:grid-cols-2">
+    <div className="sm: p-[2rem] grid grid-cols-3 gap-5 md:p-[3rem] lg:grid-cols-2">
       {bookedSeatList.map((item, index) => (
         <button
           key={index}
-          className={`w-full h-[3rem] md:h-[6rem] text-[1rem] md:text-[1.8rem]  relative border border-[#0868FD] text-white rounded-2xl`}
+          className={`w-full h-[4rem] md:h-[6rem] text-[1rem] md:text-[1.8rem]  relative border border-[#0868FD] text-white rounded-2xl`}
         >
           <i
-            className="fas fa-xmark absolute right-[-0.5rem] top-[-0.5rem] rounded-full text-[1rem] bg-[#0868FD] text-white inline-block w-[1rem]  h-[1rem] leading-[1rem]  md:w-[2rem] md:h-[2rem] md:leading-[2rem] md:text-[1.5rem]"
+            className="fas fa-xmark absolute right-[-0.5rem] top-[-0.5rem] rounded-full text-[1rem] bg-[#0868FD] text-white inline-block w-[1.2rem]  h-[1.2rem] leading-[1.2rem]  md:w-[2rem] md:h-[2rem] md:leading-[2rem] md:text-[1.5rem]"
             onClick={() => dispatch(bookSeatTicket(item))}
           ></i>
           <div className="flex flex-col h-full w-full">
