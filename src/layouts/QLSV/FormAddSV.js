daab79ed7aef2aa1e1fab565c15dc0641ca6734e
Le Nguyen Phuong Thai <thailnp133@gmail.com>
2023-01-31 16:55:43 +0700

    form quản lý sinh viên

--- src/layouts/QLSV/FormAddSV.js
+++ src/layouts/QLSV/FormAddSV.js
@@ -2,22 +2,125 @@ import React, { Component } from "react";
 import { connect } from "react-redux";
 
 class FormAddSV extends Component {
+  rexSpecialCharacter = /[~`!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?]/;
+  rexNumber = /[0-9]/;
+  rexCharacter = /[a-zA-Z]/;
   state = {
-    mssv: "",
-    fullname: "",
-    email: "",
-    phone: "",
+    values: {
+      mssv: "",
+      fullname: "",
+      email: "",
+      phone: "",
+    },
+    errors: {
+      mssv: "",
+      fullname: "",
+      email: "",
+      phone: "",
+    },
+    valid: false,
   };
+
   handleChange = (e) => {
+    let { name, value, type, pattern } = e.target;
+    let newName;
+    // ------------------BIẾN ĐỔI TOÀN BỘ SANG TIẾNG VIỆT-----------------------
+    if (name === "mssv") {
+      newName = "Mã số sinh viên";
+    } else if (name === "fullname") {
+      newName = "Họ và tên";
+    } else if (name === "phone") {
+      newName = "Số điện thoại";
+    } else {
+      newName = "Email";
+    }
+    let errorMassages = "";
+    // kiểm tra rỗng => bỎ trống đầu cuối
+    if (value.trim() === "") {
+      errorMassages = `${newName} không được bỏ trống`;
+    }
+    //---------------------KIỂM TRA MÃ SỐ SINH VIÊN----------------------
+    if (name === "mssv") {
+      if (this.rexCharacter.test(value.toString())) {
+        errorMassages = `${newName} phải là số`;
+        if (value.trim() === "") {
+          errorMassages = `${newName} không được bỏ trống`;
+        }
+      }
+      if (value.toString().match(this.rexSpecialCharacter)) {
+        errorMassages = `${newName} không được chứa ký tự đặc biệt`;
+        if (value.trim() === "") {
+          errorMassages = `${newName} không được bỏ trống`;
+        }
+      }
+    }
+    //---------------------KIỂM TRA EMAIL--------------------------
+    if (type === "email") {
+      const regex = new RegExp(pattern);
+
+      if (!regex.test(value.toString())) {
+        errorMassages = `${newName} không đúng định dạng!`;
+        if (value.trim() === "") {
+          errorMassages = `${newName} không được bỏ trống`;
+        }
+      }
+    }
+    // -------------------KIỂM TRA SỐ ĐIỆN THOẠI------------------------------------
+    if (type === "tel") {
+      const regex = new RegExp(pattern);
+      if (!regex.test(value.toString())) {
+        errorMassages = `${newName} không đúng định dạng!`;
+        if (value.trim() === "") {
+          errorMassages = `${newName} không được bỏ trống`;
+        }
+      }
+    }
+    // -------------------------KIỂM TRA HỌ VÀ TÊN------------------------------
+    if (name === "fullname") {
+      if (this.rexNumber.test(value.toString())) {
+        errorMassages = `${newName} không được chứa số`;
+        if (value.trim() === "") {
+          errorMassages = `${newName} không được bỏ trống`;
+        }
+      }
+      if (value.toString().match(this.rexSpecialCharacter)) {
+        errorMassages = `${newName} không được chứa ký tự đặc biệt`;
+        if (value.trim() === "") {
+          errorMassages = `${newName} không được bỏ trống`;
+        }
+      }
+    }
+    // cập nhật lại giá trị và lỗi cho state
+    let valuesUpdate = { ...this.state.values, [name]: value };
+    let errorsUpdate = { ...this.state.errors, [name]: errorMassages };
+    this.setState(
+      {
+        ...this.state,
+        values: valuesUpdate,
+        errors: errorsUpdate,
+      },
+      () => {
+        this.checkValid();
+      }
+    );
+  };
+  handleSubmit = (e) => {
+    e.preventDefault();
+    this.props.ThemSinhVien(this.state.values);
+  };
+  checkValid = () => {
+    let valid = true;
+    for (let key in this.state.errors) {
+      if (this.state.errors[key] !== "" || this.state.values[key] === "") {
+        valid = false;
+      }
+    }
     this.setState({
       ...this.state,
-      [e.target.name]: e.target.value,
+      valid: valid,
     });
   };
-  handleSubmit = (e)=>{
-    e.preventDefault();
-    this.props.ThemSinhVien(this.state);
-  }
+
   render() {
     return (
       <div className="my-5">
@@ -26,7 +129,7 @@ class FormAddSV extends Component {
         </h2>
         <form
           autoComplete="off"
-          className="text-white w-full grid grid-cols-2 blockSm padding-2 gap-10 p-10  bg-[#212F4F]"
+          className="text-white w-full grid grid-cols-2 blockSm padding-2 gap-12 p-10  bg-[#212F4F]"
           onSubmit={this.handleSubmit}
         >
           <div>
@@ -37,9 +140,12 @@ class FormAddSV extends Component {
               id="mssv"
               className="w-full py-5 px-10 bg-transparent border-b border-borderColor text-[2rem] text-white"
               autoFocus
-              value={this.state.mssv}
+              value={this.state.values.mssv}
               onChange={this.handleChange}
             />
+            <span className="text-red-500 italic text-[13px] md:text-[18px]">
+              {this.state.errors.mssv}
+            </span>
           </div>
           <div>
             <label htmlFor="fullname">Họ và tên:</label>
@@ -48,35 +154,52 @@ class FormAddSV extends Component {
               name="fullname"
               id="fullname"
               className="w-full py-5 px-10 bg-transparent border-b border-borderColor text-[2rem] text-white"
-              value={this.state.fullname}
+              value={this.state.values.fullname}
               onChange={this.handleChange}
             />
+            <span className="text-red-500 italic text-[13px] md:text-[18px]">
+              {this.state.errors.fullname}
+            </span>
           </div>
           <div>
             <label htmlFor="email">Email:</label>
             <input
-              type="text"
+              type="email"
               name="email"
               id="email"
               className="w-full py-5 px-10 bg-transparent border-b border-borderColor text-[2rem] text-white"
-              value={this.state.email}
+              value={this.state.values.email}
               onChange={this.handleChange}
+              pattern='^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$'
             />
+            <span className="text-red-500 italic text-[13px] md:text-[18px]">
+              {this.state.errors.email}
+            </span>
           </div>
           <div>
             <label htmlFor="phone">Số điện thoại</label>
             <input
-              type="text"
+              type="tel"
               name="phone"
               id="phone"
               className="w-full py-5 px-10 bg-transparent border-b border-borderColor text-[2rem] text-white"
-              value={this.state.phone}
+              value={this.state.values.phone}
               onChange={this.handleChange}
+              pattern="([\+84|84|0]+(3|5|7|8|9|1[2|6|8|9]))+([0-9]{8})\b"
             />
+            <span className="text-red-500 italic text-[13px] md:text-[18px]">
+              {this.state.errors.phone}
+            </span>
           </div>
           <button
             type="submit"
-            className="p-5 bg-buttonBlue font-bold rounded-xl"
+            disabled={this.state.valid ? false : true}
+            className={`p-3 md:p-5 text-[13px] md:text-[18px]  font-bold rounded-xl 
+                          ${
+                            this.state.valid
+                              ? "bg-buttonBlue cursor-pointer"
+                              : "bg-slate-500 cursor-not-allowed"
+                          }`}
           >
             Thêm sinh viên
           </button>
@@ -85,17 +208,16 @@ class FormAddSV extends Component {
     );
   }
 }
-const mapDispatchToProps = (dispatch)=>{
+const mapDispatchToProps = (dispatch) => {
   // trả về 1 sự kiện
-  return{
-    ThemSinhVien: (sinhvien)=>{
+  return {
+    ThemSinhVien: (sinhvien) => {
       const action = {
         type: "QLSV/ThemSinhVien",
         sinhvien,
       };
       dispatch(action);
-    }
-
-  }
-}
-export default connect(null,mapDispatchToProps)(FormAddSV);
+    },
+  };
+};
+export default connect(null, mapDispatchToProps)(FormAddSV);
